5f7398c996eda58702be48c5cd2adffaf353c2d8
Sam Standing <samstanding@Hallies-Air.zyxel.com>
2018-02-04 12:25:41 -0600

    added gitignore, and pulled in other files

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('sourced!');
+
+$(document).ready(function () {
+
+
+$('#catBox').on('click',showCat);
+
+
+});
+
+function showCat (e, complete) {
+  console.log('in showCat');
+  $('')
+}
