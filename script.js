38e506edf1837b9bbb58c5ef3756f744592da4c6
Sam Standing <samstanding@Hallies-Air.zyxel.com>
2018-02-04 13:45:41 -0600

    completed inventoryscripting

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-console.log('sourced!');
+var storeData = [];
 
 $(document).ready(function () {
+  //create event listener for my inventory button click
+  $('#invetoryIn').on('click', function () {
+    //new object for each new item
+    var newObject = {
+      size: $('#boxSize').val(),
+      type: $('#boxType').val(),
+      description: $('#boxDescription').val()
+    };
+    //push new data into my array
+    storeData.push(newObject);
+    //empty inputs
+    $('#boxSize').val('');
+    $('#boxType').val('');
+    $('#boxDescription').val('');
 
+    displayInventory();
+  });
 
-$('#catBox').on('click',showCat);
 
 
 });
 
-function showCat (e, complete) {
-  console.log('in showCat');
-  $('')
+//create function to display inventory items
+function displayInventory() {
+  //create a variable to target div by id
+  var output = $('.inventoryList');
+  //make sure output is fresh
+  output.empty();
+  //make a loop through stored storeData
+  for (var i = 0; i <storeData.length; i++) {
+    //add output string to list
+    outputString = '<li>';
+    outputString += storeData[i].size + ' ';
+    outputString += storeData[i].type + ' ';
+    outputString += storeData[i].description;
+    outputString += '</li>';
+    //append outputstring to outputs
+    output.append(outputString);
+  }
 }
